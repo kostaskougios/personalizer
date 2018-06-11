34517dc43bf4eb9f25f9afae4e419e972b302935
kostas.kougios@gmail.com <kostas.kougios@googlemail.com>
2018-06-11 18:36:52 +0100

    datacenter

--- common/src/main/scala/com/aktit/utils/TimeMeasure.scala
+++ common/src/main/scala/com/aktit/utils/TimeMeasure.scala
@@ -4,8 +4,25 @@ package com.aktit.utils
   * @author kostas.kougios
   *         26/05/18 - 23:45
   */
+class TimeMeasure()
+{
+	private val b = Seq.newBuilder[(String, Long)]
+
+	def dt[R](name: String)(f: => R): R = {
+		val (t, r) = TimeMeasure.dt(f)
+		b += ((name, t))
+		r
+	}
+
+	override def toString = b.result.map {
+		case (name, time) => s"$name : $time ms"
+	}.mkString("\n")
+}
+
 object TimeMeasure
 {
+	def apply() = new TimeMeasure()
+
 	/**
 	  * Measures how long f takes
 	  *
--- datacenter/src/main/scala/com/akt/personalizer/rdd/PersonalizerRDDImplicits.scala
+++ datacenter/src/main/scala/com/akt/personalizer/rdd/PersonalizerRDDImplicits.scala
@@ -3,9 +3,11 @@ package com.akt.personalizer.rdd
 import com.akt.personalizer.files.Directories
 import com.aktit.personalizer.model.TableDef
 import com.aktit.personalizer.model.time.TimeSplitter
+import com.aktit.utils.TimeMeasure
 import org.apache.hadoop.io.{BytesWritable, LongWritable}
 import org.apache.spark.SparkContext
 import org.apache.spark.api.java.StorageLevels
+import org.apache.spark.internal.Logging
 import org.apache.spark.rdd.RDD
 
 /**
@@ -13,27 +15,37 @@ import org.apache.spark.rdd.RDD
   *         10/06/18 - 17:57
   */
 
-object PersonalizerRDDImplicits
+object PersonalizerRDDImplicits extends Logging
 {
 
 	implicit class ConsumerRDD(rdd: RDD[ChannelInput])
 	{
 		def saveAsDataCenterFile[TABLE](dataDir: String, tableDef: TableDef[TABLE], timeSplitter: TimeSplitter): Unit = {
-			val persisted = rdd.persist(StorageLevels.MEMORY_AND_DISK)
-			val timesSet = persisted.map {
-				ci =>
-					timeSplitter.divideTime(ci.time)
-			}.distinct().collect().toSet
+			val timeMeasure = TimeMeasure()
 
-			for (time <- timesSet) {
-				val dt = timeSplitter.toRoundedUTCDateTime(time)
-				val path = Directories.incomingDataDirectory(dataDir, tableDef, dt)
-				persisted.filter(ci => timeSplitter.divideTime(ci.time) == time)
-					.map {
+			timeMeasure.dt("saveAsDataCenterFile") {
+				val persisted = rdd.persist(StorageLevels.MEMORY_AND_DISK)
+				val timesSet = timeMeasure.dt("collect times") {
+					persisted.map {
 						ci =>
-							(new LongWritable(ci.time), new BytesWritable(ci.data))
-					}.saveAsSequenceFile(path)
+							timeSplitter.divideTime(ci.time)
+					}.mapPartitions(_.toSet.iterator).collect().toSet
+				}
+
+				for (time <- timesSet) {
+					val dt = timeSplitter.toRoundedUTCDateTime(time)
+					val path = Directories.incomingDataDirectory(dataDir, tableDef, dt)
+					timeMeasure.dt(s"Save $path") {
+						persisted.filter(ci => timeSplitter.divideTime(ci.time) == time)
+							.map {
+								ci =>
+									(new LongWritable(ci.time), new BytesWritable(ci.data))
+							}.saveAsSequenceFile(path)
+					}
+				}
 			}
+
+			logInfo(s"Times for saveAsDataCenterFile for ${tableDef.name} :\n$timeMeasure")
 		}
 	}
 
